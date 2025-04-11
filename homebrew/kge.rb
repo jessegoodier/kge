153750af5dfdf82515adbc04ec1ba45f1c7d1a28
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2025-04-10 23:10:05 -0400

    brew package Signed-off-by: jesse goodier <31039225+jessegoodier@users.noreply.github.com>

--- homebrew/kge.rb
+++ homebrew/kge.rb
@@ -1,7 +1,7 @@
 class Kge < Formula
   include Language::Python::Virtualenv
 
-  desc "kubernetes utility for viewing pod and failed replicaset events"
+  desc "Kubernetes utility for viewing pod and failed replicaset events"
   homepage "https://github.com/jessegoodier/kge"
   url "https://github.com/jessegoodier/kge/raw/refs/heads/main/archive/refs/tags/kge-0.4.0.tar.gz"
   sha256 "9895e3e88f681cf04454b112bb4cd5c702b4833abb69f2feec4a3bb01900d383"
@@ -16,4 +16,4 @@ class Kge < Formula
   test do
     system "#{bin}/kge", "--version"
   end
-end 
+end
