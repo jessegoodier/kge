4857d274d22fc2589c2cf49468fd370971e8d666
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2025-04-10 22:43:11 -0400

    brew package Signed-off-by: jesse goodier <31039225+jessegoodier@users.noreply.github.com>

--- homebrew/kge.rb
+++ homebrew/kge.rb
@@ -2,8 +2,8 @@ class Kge < Formula
   include Language::Python::Virtualenv
 
   desc "A tool to get Kubernetes events"
-  homepage "https://github.com/jessegoodier/kge"
-  url "https://github.com/jessegoodier/kge/archive/refs/tags/v0.4.0.tar.gz"
+  homepage "https://github.com/jessegoodier/kge-kubectl-get-events"
+  url "https://github.com/jessegoodier/kge-kubectl-get-events/archive/refs/tags/v0.4.0.tar.gz"
   sha256 "3b9a5179c64edb5456d8d6d2252675df07f042710324bd24846f829dc43e9c94"
   license "MIT"
 
