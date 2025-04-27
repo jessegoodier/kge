13af94c9c724c448c04a1fb260650f9144ff2d87
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2025-04-27 16:09:24 -0400

    cleanup

--- docker-test/docker-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-podman run -it --rm -v $(pwd):/app -w /app jgoodier/ubuntu-brew:2
--- docker-test/docker-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 -m venv /tmp/venv
-source /tmp/venv/bin/activate
-pip install poetry
-poetry install
-poetry check
-poetry run pytest
